959f00a2551a13c64b834c811610e4943611bb67
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-05-08 20:18:18 +0300

    Enhance API middleware, JSON options, and Swagger setup

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -66,7 +66,6 @@ namespace SchoolApp
                 };
             });
 
-
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowClient", policy =>
@@ -107,21 +106,18 @@ namespace SchoolApp
                 options.OperationFilter<AuthorizeOperationFilter>();
             });
 
-            // Add services to the container.
-
-
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            //builder.Services.AddOpenApi();
-
-
-
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+            builder.Services.AddProblemDetails();
 
             var app = builder.Build();
+
+            app.UseExceptionHandler();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                //app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
